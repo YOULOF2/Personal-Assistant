09bb2aa70a363991dafec334fce22e2abdf4ccbf
Youssef Khaled <46977403+YOULOF2@users.noreply.github.com>
2022-03-31 18:14:33 +0200

    place all code inside 'src' folder

--- src/backend/__init__.py
+++ src/backend/__init__.py
@@ -1,7 +1,6 @@
 from wikitionary_lookup import dictionary_lookup
 from wikipedia_lookup import wikipedia_search
 from weather import get_weather
-from backend.youtube_search import search_youtube, return_video_url_youtube, return_audio_url_youtube
 from todo_list import ToDoList, ToDoItem
 from jokes import get_joke
 from trivia_db import get_trivia
